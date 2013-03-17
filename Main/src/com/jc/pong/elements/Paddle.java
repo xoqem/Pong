63c3f2bc39e985dd36c49d8f0c800610d4b15821
xoqem <sinfree@gmail.com>
2013-03-17 14:02:19 -0700

    Removes comments

--- Main/src/com/jc/pong/elements/Paddle.java
+++ Main/src/com/jc/pong/elements/Paddle.java
@@ -17,32 +17,23 @@ public class Paddle {
   //---------------------------------------------------------------------------
 
   public Paddle(World world, Vector2 position, Vector2 size) {
-    // First we create a body definition
     BodyDef bodyDef = new BodyDef();
-    // We set our body to dynamic, for something like ground which doesnt move we would set it to StaticBody
     bodyDef.type = BodyType.KinematicBody;
-    // Set our body's starting position in the world
     bodyDef.position.set(position);
 
-    // Create our body in the world using our body definition
     body = world.createBody(bodyDef);
 
-    // Create a circle shape and set its radius to 6
     PolygonShape rect = new PolygonShape();
     rect.setAsBox(size.x, size.y);
 
-    // Create a fixture definition to apply our shape to
     FixtureDef fixtureDef = new FixtureDef();
     fixtureDef.shape = rect;
     fixtureDef.density = 0.5f;
     fixtureDef.friction = 0f;
-    fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+    fixtureDef.restitution = 0.6f;
 
-    // Create our fixture and attach it to the body
     Fixture fixture = body.createFixture(fixtureDef);
 
-    // Remember to dispose of any shapes after you're done with them!
-    // BodyDef and FixtureDef don't need disposing, but shapes do.
     rect.dispose();
   }
 
--- Main/src/com/jc/pong/elements/Puck.java
+++ Main/src/com/jc/pong/elements/Puck.java
@@ -17,32 +17,23 @@ public class Puck {
   //---------------------------------------------------------------------------
 
   public Puck(World world, Vector2 position, float radius) {
-    // First we create a body definition
     BodyDef bodyDef = new BodyDef();
-    // We set our body to dynamic, for something like ground which doesnt move we would set it to StaticBody
     bodyDef.type = BodyType.DynamicBody;
-    // Set our body's starting position in the world
     bodyDef.position.set(position);
 
-    // Create our body in the world using our body definition
     body = world.createBody(bodyDef);
 
-    // Create a circle shape and set its radius to 6
     CircleShape circle = new CircleShape();
     circle.setRadius(radius);
 
-    // Create a fixture definition to apply our shape to
     FixtureDef fixtureDef = new FixtureDef();
     fixtureDef.shape = circle;
     fixtureDef.density = 0.5f;
     fixtureDef.friction = 0.15f;
-    fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+    fixtureDef.restitution = 0.6f;
 
-    // Create our fixture and attach it to the body
     Fixture fixture = body.createFixture(fixtureDef);
 
-    // Remember to dispose of any shapes after you're done with them!
-    // BodyDef and FixtureDef don't need disposing, but shapes do.
     circle.dispose();
   }
 
